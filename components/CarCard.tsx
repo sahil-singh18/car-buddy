41ec1e68882d550af21a49e71fc5439d140ecb30
sahil-singh18 <sahilsingh18may2004@gmail.com>
2023-11-22 11:50:14 +0530

    initial commit

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -60,7 +60,7 @@ const CarCard = ({ car }: CarCardProps) => {
              </div>
            </div>
               
-            <div className="car-card__btn-container">
+            <div className="car-card__btn-container xs:block">
                 <CustomButton 
                   title="View More"
                   containerStyles="w-full py-[16px] rounded-full bg-primary-blue"
--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -49,8 +49,8 @@ const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacture
             leaveTo='opacity-0'
             afterLeave={() => setQuery("")} 
           >
-            <Combobox.Options
-              className='absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm'
+            <Combobox.Options 
+              className='absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm z-40' 
               static
             >
               {filteredManufacturers.length === 0 && query !== "" ? (
